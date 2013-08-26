3b1a976d9af4290ecf753d5b9289df4c6c345e67
sergey <sergey@localhost.localdomain>
2013-08-27 01:51:01 +0300

    Fixed problems with fields "model", "os_version", "app_version" Added support proxy-server on server side.

--- anno_gae_web_service/main.py
+++ anno_gae_web_service/main.py
@@ -5,5 +5,7 @@ Created on Jun 13, 2013
 '''
 import webapp2
 from sync import AnnoSync
+from ReverseProxyServer import ReverseProxyServer
 
 application = webapp2.WSGIApplication([('/', AnnoSync), ('/sync', AnnoSync)], debug=True)
+application = ReverseProxyServer(application)
