f419ee7b05cb7c0143ac1eb160e82e9cd1837c67
David Lee <allmywant@gmail.com>
2013-09-04 16:35:56 +0800

    enabling the spiral gesture in Community activity

--- anno_plugin_android/src/co/usersource/annoplugin/view/CommunityActivity.java
+++ anno_plugin_android/src/co/usersource/annoplugin/view/CommunityActivity.java
@@ -1,15 +1,33 @@
 package co.usersource.annoplugin.view;
 
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import co.usersource.annoplugin.AnnoPlugin;
 import org.apache.cordova.DroidGap;
+import android.gesture.GestureOverlayView;
 
 import android.os.Bundle;
 
-public class CommunityActivity extends DroidGap {
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    super.loadUrl("file:///android_asset/www/pages/community/main.html");
-  }
+public class CommunityActivity extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.init();
+
+        GestureOverlayView view = new GestureOverlayView(this);
+        view.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1));
+
+        setContentView(view);
+        view.addView((View) appView.getParent()); //adds the PhoneGap browser
+        view.getChildAt(0).setLayoutParams(new FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT, 1));
+
+        setContentView(view);
+        AnnoPlugin.setEnableGesture(this, view, true);
+        super.loadUrl("file:///android_asset/www/pages/community/main.html");
+    }
 
 }
