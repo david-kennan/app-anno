be4000b6d4ab18d5bfe46cf488e015d83613d65f
David Lee <allmywant@gmail.com>
2013-09-04 16:34:46 +0800

    add a new setEnableGesture method accept GestureOverlayView  parameter

--- anno_plugin_android/src/co/usersource/annoplugin/AnnoPlugin.java
+++ anno_plugin_android/src/co/usersource/annoplugin/AnnoPlugin.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package co.usersource.annoplugin;
 
@@ -9,31 +9,56 @@ import co.usersource.annoplugin.gesture.ScreenshotGestureListener;
 
 /**
  * Anno plugin entry.
- * 
+ *
  * @author topcircler
- * 
  */
-public class AnnoPlugin {
+public class AnnoPlugin
+{
 
-  /**
-   * Enable taking screenshot by certain gesture.
-   * 
-   * @param activity
-   * @param gestureViewId
-   */
-  public static void setEnableGesture(Activity activity, int gestureViewId,
-      boolean enabled) {
-    GestureOverlayView gestureOverlayView = (GestureOverlayView) activity
-        .findViewById(gestureViewId);
-    if (enabled) {
-      gestureOverlayView.setGestureVisible(false);
-      ScreenshotGestureListener gesturePerformedListener = new ScreenshotGestureListener(
-          activity, R.raw.gestures);
-      gestureOverlayView
-          .addOnGesturePerformedListener(gesturePerformedListener);
-    } else {
-      gestureOverlayView.removeAllOnGesturePerformedListeners();
+    /**
+     * Enable taking screenshot by certain gesture.
+     *
+     * @param activity
+     * @param gestureViewId
+     */
+    public static void setEnableGesture(Activity activity, int gestureViewId,
+                                        boolean enabled)
+    {
+        GestureOverlayView gestureOverlayView = (GestureOverlayView) activity
+                .findViewById(gestureViewId);
+        if (enabled)
+        {
+            gestureOverlayView.setGestureVisible(false);
+            ScreenshotGestureListener gesturePerformedListener = new ScreenshotGestureListener(
+                    activity, R.raw.gestures);
+            gestureOverlayView
+                    .addOnGesturePerformedListener(gesturePerformedListener);
+        } else
+        {
+            gestureOverlayView.removeAllOnGesturePerformedListeners();
+        }
     }
-  }
+
+    /**
+     * Enable taking screenshot by certain gesture.
+     *
+     * @param activity
+     * @param gestureOverlayView
+     */
+    public static void setEnableGesture(Activity activity, GestureOverlayView gestureOverlayView,
+                                        boolean enabled)
+    {
+        if (enabled)
+        {
+            gestureOverlayView.setGestureVisible(false);
+            ScreenshotGestureListener gesturePerformedListener = new ScreenshotGestureListener(
+                    activity, R.raw.gestures);
+            gestureOverlayView
+                    .addOnGesturePerformedListener(gesturePerformedListener);
+        } else
+        {
+            gestureOverlayView.removeAllOnGesturePerformedListeners();
+        }
+    }
 
 }
